410e833076b25f7cf17956244e1213180d4b1c14
DavidH102 <david@britecit.com>
2024-11-16 16:20:55 -0500

    Code cleanup and minor updates to various files
    Reformatted `FluxEstates.csproj` for indentation consistency.
    Removed unnecessary `using` directives and reformatted `namespace` in `Commercial.cs`.
    Updated `CurrentEstateObject` and `ComponentName` to be nullable in `EstateTypeRenderer.razor`.
    Specified type parameter `T` as `string` for `MudChip` in `SavedEstates.razor`.

--- Models/Abstract/Commercial.cs
+++ Models/Abstract/Commercial.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FluxEstates.Models.Abstract
+﻿namespace FluxEstates.Models.Abstract
 {
     public abstract class Commercial : Estate
     {
